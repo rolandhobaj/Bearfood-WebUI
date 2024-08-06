8957b5c1a8cb3e19e1f9396527eb62fb921b0d8f
rolandhobaj <rolandhobaj@gmail.com>
2024-08-06 10:05:21 +0200

    Make login form a card

--- bearfood-webui/src/App.tsx
+++ bearfood-webui/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LoginForm from './Login/LoginForm';
 
+
 function App() {
   return (
     <div className="App">
--- bearfood-webui/src/Login/LoginForm.tsx
+++ bearfood-webui/src/Login/LoginForm.tsx
@@ -1,19 +1,23 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { Card } from '@mui/material';
+import { Box } from '@mui/material';
+import exampleImage from '../assets/icon.png'
 
 function LoginForm(){
     return (
-        <Stack sx={stackStyle}>    
+        <Box sx={boxStyle}>
+        <Card sx={cardStyle}>
+        <img src={exampleImage} alt="Example" width='200' height='auto'/>
 
-            <h1>Welcome to Bearfood!</h1>
+        <Stack>
             <TextField className='TextField' id="outlined-basic" label="Username" required variant="outlined" sx={textFieldStyles} />
             <TextField className='TextField' id="outlined-basic" label="Password" required variant="outlined" sx={textFieldStyles}/>
-            <Stack spacing={2} direction="row">
-                <Button variant="contained">Login</Button>
-                <Button variant="contained">Register</Button>
-            </Stack>
+            <Button sx={buttonStyle} variant="contained">Login</Button>
         </Stack>
+        </Card>
+        </Box>
     );
 }
 
@@ -21,11 +25,25 @@ const textFieldStyles = {
     margin: '5px'
 };
 
-const stackStyle = {
-    height: '100vh',
-    display: 'flex',
-    'align-items': 'center',
-    'justify-content': 'center'
+const buttonStyle = {
+    'margin-top': '30px',
+    'background-color': 'rgba(131,165,173, 1)'
 };
 
+const cardStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width : '30%',
+    height: '70%',
+    padding: '20px'
+};
+
+const boxStyle = {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
 export default LoginForm;
