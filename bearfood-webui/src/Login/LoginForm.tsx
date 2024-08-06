444eb6232c70fb91ff5145542a998f58dfcc9af0
rolandhobaj <rolandhobaj@gmail.com>
2024-08-06 11:39:54 +0200

    Add password textbox

--- bearfood-webui/src/Login/LoginForm.tsx
+++ bearfood-webui/src/Login/LoginForm.tsx
@@ -1,20 +1,51 @@
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { Card } from '@mui/material';
-import { Box } from '@mui/material';
+import { Card, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@mui/material';
+import { Box, FormControl } from '@mui/material';
 import exampleImage from '../assets/icon.png'
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function LoginForm(){
+    const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
     return (
         <Box sx={boxStyle}>
         <Card sx={cardStyle}>
-        <img src={exampleImage} alt="Example" width='200' height='auto'/>
+        <img src={exampleImage} alt="Example" width='180' height='auto'/>
 
         <Stack>
             <TextField className='TextField' id="outlined-basic" label="Username" required variant="outlined" sx={textFieldStyles} />
-            <TextField className='TextField' id="outlined-basic" label="Password" required variant="outlined" sx={textFieldStyles}/>
+            <FormControl sx={textFieldStyles} variant="outlined">
+             <InputLabel htmlFor="outlined-adornment-password">Password *</InputLabel>
+             <OutlinedInput
+               id="outlined-adornment-password"
+               type={showPassword ? 'text' : 'password'}
+               endAdornment={
+                 <InputAdornment position="end">
+                   <IconButton
+                     aria-label="toggle password visibility"
+                     onClick={handleClickShowPassword}
+                     onMouseDown={handleMouseDownPassword}
+                     edge="end"
+                   >
+                     {showPassword ? <VisibilityOff /> : <Visibility />}
+                   </IconButton>
+                 </InputAdornment>
+               }
+               label="Password"
+             />
+            </FormControl>
             <Button sx={buttonStyle} variant="contained">Login</Button>
+            <p> Not a member yet? Sign up here!</p>
         </Stack>
         </Card>
         </Box>
